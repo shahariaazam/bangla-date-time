dd5d6e89700af2afb43698d1753a8a30f3c4fb97
Shaharia Azam <shaharia.azam@gmail.com>
2012-09-29 16:26:26 +0600

    Add character in application info on comment line on top

--- BanglaDateTime.php
+++ BanglaDateTime.php
@@ -2,7 +2,7 @@
 /*	Translate English Date Time in Bengali
 *		V1.0
 
-*		Author: Shaharia Azam 
+*		Author: Shaharia Azam s
 *		Author URL: http://www.shahariaazam.com
 */
 class ShowBanglaDateTime{
