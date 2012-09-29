141421c3d1222169b0796a00a9cbb9aa70c72ae1
Shaharia Azam <shaharia.azam@gmail.com>
2012-09-29 16:42:38 +0600

    Base function created

--- BanglaDateTime.php
+++ BanglaDateTime.php
@@ -1,10 +1,14 @@
 <?php
-/*	Translate English Date Time in Bengali
-*		V1.0
-
-*		Author: Shaharia Azam s
-*		Author URL: http://www.shahariaazam.com
-*/
+/*  Show English Date Time in Bengali
+ * Version: 1.0
+ * Description: Show bangla date time like the following
+ * Author: Shaharia Azam
+ * Author URL: http://www.shahariaazam.com
+ */
 class ShowBanglaDateTime{
+    //Base function
+    public function ShowBanglaDateTime($time=null,$type=null,$additional=null){
+        
+    }
 }
 ?>
